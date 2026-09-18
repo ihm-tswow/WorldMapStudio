b91f1f817ff66475e78585d010154d304c929afa
ihm-tswow <ihm-ts@protonmail.com>
2026-09-18 18:13:23 +0200

    docs: add page on running tests from scripts

--- docs-site/sidebars.ts
+++ docs-site/sidebars.ts
@@ -1,7 +1,7 @@
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-  tutorialSidebar: ['intro', 'terrain-materials', 'editor-style'],
+  tutorialSidebar: ['intro', 'terrain-materials', 'editor-style', 'running-tests'],
 };
 
 export default sidebars;
